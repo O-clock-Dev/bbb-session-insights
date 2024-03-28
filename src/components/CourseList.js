69a822539fe17b9cecbeb8189f1cb2ef2b0075a7
Alex (HDG) <alexandre.ollivier@protonmail.com>
2024-03-28 16:11:56 +0100

    fix: URL in courselist

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -3,7 +3,7 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function CourseList() {
-  const { data, error } = useSWR("/api/generate-courses-list", fetcher, {
+  const { data, error } = useSWR("dashboards/api/generate-courses-list", fetcher, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
     revalidateIfStale: false,
